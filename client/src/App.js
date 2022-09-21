56bcd63cc42a20d8c21cf6bfc1cba887634340c5
E132 <107452563+132E@users.noreply.github.com>
2022-09-21 13:19:23 -0500

    about me section up

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
+import About from './components/About'
 
 const App = () => {
   return (
     <div>
       <NavBar />
       <Home />
+      <About />
     </div>
   );
 };
---
+++ client/src/components/About.jsx
@@ -0,0 +1,24 @@
+const About = () => {
+    return(
+        <div name="about" className="w-full h-screen bg-[#0a192f] text-gray-300">
+            <div className='flex flex-col justify-center items-center w-full h-full'>
+                <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8">
+                    <div className="sm:text-right pb-8 pl-4">
+                        <p className="text-4xl font-bold inline border-b-4 border-[#fe0068]">About</p>
+                    </div>
+                    <div></div>
+                </div>
+                <div className='max-w[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
+                    <div className="sm:text-right text-4xl font-bold">
+                        <p>Hello! I'm Eric, nice to meet you. Please take a lurk around.</p>
+                    </div>
+                    <div>
+                        <p>I am a husband and father learning to build software that will hopefully improve the lives of those around me. My passion for design and creative problem solving gives me an advantange when develping software. </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default About
