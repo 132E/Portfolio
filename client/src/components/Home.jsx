f26f6d06e64b74b930807863efeb783e3c5f4037
E132 <107452563+132E@users.noreply.github.com>
2022-09-21 12:37:30 -0500

    home page and nav functionality working

---
+++ client/src/components/Home.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import {HiArrowNarrowRight} from 'react-icons/hi';
+
+const Home = () => {
+    return (
+        <div name="home" className="w-full h-screen bg-[#0a192f]">
+            {/* container */}
+            <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full ">
+                <p className="text-[#fe0068]">Hi my name is</p>
+                <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">Eric Deleon</h1>
+                <h2 className="text-4xl sm:text-7xl font-bold text-[#6c7cab]">I'm a Full Stack Developer.</h2>
+                <p className="text-[#6c7cab] py-4 max-w-[700px]">I’m a full-stack developer with a focus on front-end development. As a Creative Director, I apply my passion for visual storytelling and UXUI design to my work as a developer. I use my comprehensive knowledge of digital and print applications to create robust processes and implement best practices. Let's take a journey together and develop something cool along the way.</p>
+                <div>
+                    <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#fe0068] hover:border-[#fe0068]">
+                        Peep the Steez
+                        <span className="group-hover:rotate-90 duration-300">
+                            <HiArrowNarrowRight className="ml-3"/>
+                        </span>
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default Home;
