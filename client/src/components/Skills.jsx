9b612f221d436235ac5b499b42f0702f7421e11d
E132 <107452563+132E@users.noreply.github.com>
2022-09-22 09:08:45 -0500

    made adjustmens to skills section. still needs other logos ans CSS

--- client/src/components/Skills.jsx
+++ client/src/components/Skills.jsx
@@ -14,7 +14,8 @@ import Tailwind from '../assets/tailwind.png';
 
 const Skills = () => {
     return (
-        <div name="skills">
+        <div name="skills" className="w-full h-screen bg-[#0a192f] text-gray-300">
+            
             {/* container */}
             <div>
                 <div>
