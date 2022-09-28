ba958e57a997d6e0830166999f9fff0d68345783
E132 <107452563+132E@users.noreply.github.com>
2022-09-28 08:51:40 -0500

    MVP Complete

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -56,11 +56,31 @@ const NavBar = () => {
 
             {/* Mobile Menu */}
             <ul className={!nav ? 'hidden':'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
-                <li className='py-6 text-4xl'>Home</li>
-                <li className='py-6 text-4xl'>About</li>
-                <li className='py-6 text-4xl'>Skills</li>
-                <li className='py-6 text-4xl'>Portfolio</li>
-                <li className='py-6 text-4xl'>Contact</li>
+                <li className='py-6 text-4xl'>
+                    <Link onClick={handleClick} to="home" smooth={true} offset={50} duration={500} >
+                        Home
+                    </Link>
+                </li>
+                <li className='py-6 text-4xl'>
+                    <Link onClick={handleClick} to="about" smooth={true} offset={50} duration={500} >
+                        About
+                    </Link>
+                </li>
+                <li className='py-6 text-4xl'>
+                    <Link onClick={handleClick} to="skills" smooth={true} offset={50} duration={500} >
+                        Skills
+                    </Link>
+                </li>
+                <li className='py-6 text-4xl'>
+                    <Link onClick={handleClick} to="work" smooth={true} offset={50} duration={500} >
+                        Portfolio
+                    </Link>
+                </li>
+                <li className='py-6 text-4xl'>
+                    <Link onClick={handleClick} to="contact" smooth={true} offset={50} duration={500} >
+                        Hit Me Up
+                    </Link>
+                </li>
             </ul>
 
             {/* Social Icons */}
